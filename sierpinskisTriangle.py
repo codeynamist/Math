62c66169882f7df5f357e8855044456dec3e9a56
codeynamics <120318387+codeynamics@users.noreply.github.com>
2023-04-18 21:42:18 +0530

    Update sierpinskisTriangle.py

--- sierpinskisTriangle.py
+++ sierpinskisTriangle.py
@@ -1,7 +1,7 @@
 #This code generates Sierpinski's triangle when given 4 inputs. 3 inputs in cartesian coordinates (p,x),(q,y),(r,z).
 #Or if you want an equilateral triangle, you can just specify xx, which is the max value in x and y coordinates.
 #Then 1 more inout is given (j,k), which is the initial guess point. 
-#by default, the no. of iterations is taken as 100000, you can change it as you like.
+#By default, the no. of iterations is taken as 100000, you can change it as you like.
 
 #The concept behind the working of this code is that, 3 initial points are taken in a cartesian space and a 4th point is assumed in a random place.
 #Now a random point out of the initial 3 points is taken and the mid point between the taken random point and chosen 4th point is the new 4th point. 
@@ -9,6 +9,7 @@
 #This process is repeated for all iterations and this will give a Sierpinski's triangle Fractal. 
 
 #The beauty is that, even if you take random guess very far away from the initial triangle, the fractal triangle would still form. 
+#The code generates the fractal even if the initial guess is in a region where the points should actually not lie.
 
 import random
 from matplotlib import pyplot as plt
